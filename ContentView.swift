e28799881e7fd3e3128f1fdf01b4139adc6d8de3
Griffin Tang <gltang@andrew.cmu.edu>
2021-02-25 20:55:36 -1000

    initial work

---
+++ ContentView.swift
@@ -0,0 +1,56 @@
+//
+//  ContentView.swift
+//  HopOn
+//
+//  Created by Griffin Tang on 2/25/21.
+//
+
+import SwiftUI
+
+enum Score: Int, CaseIterable {
+    case one = 1
+    case two
+    case three
+    case four
+    case five
+}
+
+
+//@TODO figure out firebase unique ID & date system
+struct Rating {
+    var ratingId: String = ""
+    var time: Date
+    var userId: String = ""
+    var drinkId: String = ""
+    var score: Score = .one
+    var critique: String = ""
+}
+
+//@TODO make type into filterable nested types
+struct Drink {
+    var drink_id: String = ""
+    var name: String = ""
+    var drinkType: String = ""
+    
+    
+}
+
+struct ContentView: View {
+    @State var rating: Rating
+    var body: some View {
+        print(rating.drinkId)
+        return VStack {
+            TextField("Beer Name", text: $rating.drinkId)
+            TextField("Beer Name", text: $rating.drinkId)
+            TextField("Beer Name", text: $rating.drinkId)
+            TextField("Beer Name", text: $rating.drinkId)
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView(rating: Rating())
+    }
+}
---
+++ HopOnApp.swift
@@ -0,0 +1,17 @@
+//
+//  HopOnApp.swift
+//  HopOn
+//
+//  Created by Griffin Tang on 2/25/21.
+//
+
+import SwiftUI
+
+@main
+struct HopOnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView(rating: Rating())
+        }
+    }
+}
